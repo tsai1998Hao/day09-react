8c5ebd2b0bb5b62bde3fca0c371623d7378d2b94
tsailearn87code <142395260+tsailearn87code@users.noreply.github.com>
2024-11-18 22:40:58 +0800

    delete useless document and change the way path

--- pages/add_emp_data.js
+++ pages/add_emp_data.js
@@ -34,7 +34,7 @@ export default function Add_emp_data() {
         console.log(password_value);
         console.log(store_value);
         console.log(permission_value);
-        // console.log("請求一次");
+        console.log("請求一次");
     
         fetch('http://localhost/%e5%81%b7%e7%b7%b4/day09/day09_api02.php',{
           method:'POST',
@@ -50,13 +50,13 @@ export default function Add_emp_data() {
           })
         })
         .then(res=>{
-          console.log(res);
+          // console.log(res);
           return  res.json()
         })
           
         .then(result=>{
           console.log(result)
-          location.reload();
+          // location.reload();
         })
       }, [account_value, name_value, password_value, store_value, permission_value]);
     
